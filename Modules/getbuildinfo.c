3b579aeb401290e483bbe2c6e0d26f4679d2bf8a
Andrew Kelley <andrew@ziglang.org>
2024-08-30 19:14:59 -0700

    avoid unreproducible date and time macros
    expansion of date or time macro is not reproducible

--- Modules/getbuildinfo.c
+++ Modules/getbuildinfo.c
@@ -5,19 +5,11 @@
 #endif
 
 #ifndef DATE
-#ifdef __DATE__
-#define DATE __DATE__
-#else
 #define DATE "xx/xx/xx"
-#endif
 #endif
 
 #ifndef TIME
-#ifdef __TIME__
-#define TIME __TIME__
-#else
 #define TIME "xx:xx:xx"
-#endif
 #endif
 
 /* XXX Only unix build process has been tested */
